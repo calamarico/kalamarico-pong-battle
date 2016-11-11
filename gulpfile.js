df33d5c28c45c830a98ac9b22241e5c984375bdf
calamarico <daniel.hernandez.zafra@gmail.com>
2016-11-11 18:41:18 +0100

    ADD gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
-var gulp = require('gulp');
+const gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    watch = require('gulp-watch');
 
-gulp.task('default', function() {
+gulp.task('lint', function() {
+  return gulp.src('./core/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('watch', function() {
+     return watch('./core/*.js', function() {
+        gulp.run('lint');
+     });
 });
