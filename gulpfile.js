87f10738af4cb517b75bf0aea6935f6dd3f6e71d
calamarico <daniel.hernandez.zafra@gmail.com>
2016-11-11 18:42:03 +0100

    CHANGE gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ const gulp = require('gulp'),
     stylish = require('jshint-stylish'),
     watch = require('gulp-watch');
 
+// Lint task
 gulp.task('lint', function() {
   return gulp.src('./core/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
+// Watch task. Call to lint task.
 gulp.task('watch', function() {
      return watch('./core/*.js', function() {
         gulp.run('lint');
